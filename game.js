f817ccbc38d2ec6a5c613911de510a4c86267d73
Erin Carmona <carmona.erin.u@gmail.com>
2023-09-13 22:38:11 -0600

    Se inplementan intervalos de tiempo del jugardor y contador de vidas

--- game.js
+++ game.js
@@ -4,10 +4,17 @@ const btnUp = document.querySelector("#up");
 const btnLeft = document.querySelector("#left");
 const btnRight = document.querySelector("#right");
 const btnDown = document.querySelector("#down");
-
+const spanLives = document.querySelector("#lives");
+const spanTime = document.querySelector("#time");
 
 let canvasSize;
 let elementsSize; 
+let level = 0;
+let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
 
 const playerPosition = {
     x: undefined,
@@ -17,6 +24,7 @@ const giftPosition = {
     x: undefined,
     y: undefined,
 }
+let enemyPositions = [];
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -42,11 +50,23 @@ function startGame() {
     game.font = elementsSize + "px Verdana";
     game.textAlign = "end";
 
-    const map = maps[1];
-    const mapRows = map.trim().split("\n");
-    const mapRowCols = mapRows.map(row => row.trim().split(""))
-    console.log({map, mapRows, mapRowCols});
+    const map = maps[level];
 
+        if (!map) {
+        gameWin();
+        return;
+}
+        if (!timeStart) {
+            timeStart = Date.now();
+            timeInterval = setInterval(showTime, 100);
+        }
+
+    const mapRows = map.trim().split("\n");
+    const mapRowCols = mapRows.map(row => row.trim().split(""));
+
+    showLives();
+
+    enemyPositions = [];
     game.clearRect(0,0, canvasSize, canvasSize);
 
     mapRowCols.forEach( (row, rowI)=> {
@@ -63,6 +83,11 @@ function startGame() {
             } else if (col == "I") {
                 giftPosition.x = posX;
                 giftPosition.y = posY;
+            } else if (col == "X") {
+                enemyPositions.push({
+                    x: posX,
+                    y: posY,
+                })
             }
 
             game.fillText(emoji, posX, posY);
@@ -73,15 +98,63 @@ function startGame() {
 }
 
 function movePlayer() {
-    const giftCollisionX = playerPosition.x.toFixed(5) == giftPosition.x.toFixed(5);
-    const giftCollisionY = playerPosition.y.toFixed(5) == giftPosition.y.toFixed(5);
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
     const giftCollision = giftCollisionX && giftCollisionY;
 
     if (giftCollision) {
-        console.log("Pasaste");
+        levelWin();
     }
 
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    }); 
+    if (enemyCollision) {
+       levelFail();
+    }
+
+
     game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function levelWin() {
+    console.log("pasastes");
+    level++;
+    startGame();
+}
+
+function levelFail() {
+    console.log("Mamaste");
+    lives--;     
+
+    console.log(lives);
+    if (lives <= 0) {
+        level = 0; 
+        lives = 3;
+        timeStart = undefined;
+    }
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
+    startGame();    
+}
+
+function gameWin() {
+    console.log("Eres el campeon");
+    clearInterval(timeInterval);
+}
+
+function showLives() {
+    const heartsArray = Array(lives).fill(emojis["HEART"]) ;
+
+    spanLives.innerHTML = "";
+    heartsArray.forEach(heart => spanLives.append(heart));
+
+}
+
+function showTime() {
+    spanTime.innerHTML = Date.now() - timeStart;
 }
 
 window.addEventListener("keydown", moveByKeys);
